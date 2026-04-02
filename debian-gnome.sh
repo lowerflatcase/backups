1d4bacc900d0b848e935c2e10e4b470d3fc83333
Han <253545469+lowerflatcase@users.noreply.github.com>
2026-04-03 00:47:16 +0530

    asdf

--- debian-gnome.sh
+++ debian-gnome.sh
@@ -3,7 +3,7 @@
 exec 2> >(tee -a setup.log >&2)
 
 remove_bloat_packages() {
-    sudo apt purge -y baobab evolution firefox-esr gnome-calculator gnome-calendar gnome-characters gnome-clocks gnome-connections gnome-contacts gnome-disk-utility gnome-logs gnome-maps gnome-music gnome-sound-recorder gnome-system-monitor gnome-tour gnome-weather libreoffice* malcontent seahorse shotwell simple-scan totem yelp
+    sudo apt purge -y baobab evolution firefox-esr gnome-calculator gnome-calendar gnome-characters gnome-clocks gnome-connections gnome-contacts gnome-disk-utility gnome-logs gnome-maps gnome-music gnome-sound-recorder gnome-system-monitor gnome-tour gnome-weather 'libreoffice*' malcontent seahorse shotwell simple-scan totem yelp
 }
 
 apt_system_refresh() {
@@ -15,7 +15,11 @@ enable_i386_architecture() {
 }
 
 install_base_packages() {
-    sudo apt update && sudo apt install -y adb curl ffmpeg flatpak flatpak-xdg-utils gir1.2-flatpak-1.0 libflatpak0 fonts-atkinson-hyperlegible fonts-inter fonts-jetbrains-mono fonts-noto-color-emoji fonts-noto-core gh git gnome-software-plugin-flatpak libnotify-bin libnotify4 libnss3 libxcb-cursor0 libxcb-xinerama0 nodejs npm pypy3 pypy3-venv python3 python3-venv tcpdump tlp tlp-rdw tree virt-manager wget zstd
+    sudo apt update && sudo apt install -y adb curl ffmpeg flatpak flatpak-xdg-utils gir1.2-flatpak-1.0 libflatpak0 fonts-atkinson-hyperlegible fonts-inter fonts-jetbrains-mono fonts-noto-color-emoji fonts-noto-core gh git gnome-software-plugin-flatpak libnotify-bin libnotify4 libnss3 libxcb-cursor0 libxcb-xinerama0 nodejs npm pypy3 pypy3-venv python3 python3-venv tcpdump tlp tlp-rdw tree virt-manager zstd wget gpg apt-transport-https gvfs libglib2.0-bin
+}
+
+increase_file_watch_limit(){
+    echo "fs.inotify.max_user_watches=524288" | sudo tee -a /etc/sysctl.conf && sudo sysctl --system
 }
 
 restore_grub_configuration() {
@@ -30,16 +34,17 @@ add_repo_brave_browser() {
     sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg && sudo curl -fsSLo /etc/apt/sources.list.d/brave-browser-release.sources https://brave-browser-apt-release.s3.brave.com/brave-browser.sources
 }
 
-add_repo_vscodium() {
-    wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg | gpg --dearmor | sudo dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg && echo -e 'Types: deb\nURIs: https://download.vscodium.com/debs\nSuites: vscodium\nComponents: main\nArchitectures: amd64 arm64\nSigned-by: /usr/share/keyrings/vscodium-archive-keyring.gpg' | sudo tee /etc/apt/sources.list.d/vscodium.sources
-}
-
 add_repo_syncthing() {
     sudo mkdir -p /etc/apt/keyrings && sudo curl -L -o /etc/apt/keyrings/syncthing-archive-keyring.gpg https://syncthing.net/release-key.gpg && echo "deb [signed-by=/etc/apt/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing stable-v2" | sudo tee /etc/apt/sources.list.d/syncthing.list
 }
 
 add_repo_flathub() {
     flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+}
+
+add_repo_vscode(){
+    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /usr/share/keyrings/microsoft.gpg > /dev/null
+    echo "deb [arch=amd64,arm64,armhf signed-by=/usr/share/keyrings/microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
 }
 
 install_anki_package() {
@@ -55,15 +60,15 @@ setup_virtualization() {
 }
 
 install_additional_packages() {
-    sudo apt-get install -y brave-browser codium syncthing proton-vpn-gnome-desktop
+    sudo apt-get install -y brave-browser codium syncthing proton-vpn-gnome-desktop code
 }
 
 install_flatpak_packages() {
     flatpak install -y flathub org.kde.kdenlive md.obsidian.Obsidian io.mrarm.mcpelauncher
 }
 
-restore_vscodium_configuration() {
-    mkdir -p ~/.config/VSCodium/User && cp -f setting-codium ~/.config/VSCodium/User/settings.json
+restore_vscode_configuration(){
+    mkdir -p ~/.config/Code/User && cp -f setting-code ~/.config/Code/User/settings.json
 }
 
 restore_tlp_configuration() {
@@ -79,9 +84,16 @@ setup_git_configuration() {
 }
 
 clone_repo(){
-    sudo apt install git && git clone https://github.com/lowerflatcase/backups.git && cd backups/scripts/
+    sudo apt install -y git && git clone https://github.com/lowerflatcase/backups.git && cd backups/scripts/
 }
 
+pin_vscode_repo() {
+    sudo tee /etc/apt/preferences.d/code <<EOF > /dev/null
+Package: code
+Pin: origin "packages.microsoft.com"
+Pin-Priority: 9999
+EOF
+}
 
 
 clone_repo
@@ -92,19 +104,22 @@ restore_grub_configuration
 setup_git_configuration
 install_battery_alert_service
 
-add_repo_brave_browser
-add_repo_vscodium
-add_repo_syncthing
-add_repo_flathub
 apt_system_refresh
 install_base_packages
+add_repo_brave_browser
+add_repo_syncthing
+add_repo_flathub
+add_repo_vscode
+pin_vscode_repo
+apt_system_refresh
 install_anki_package
 install_additional_packages
 install_flatpak_packages
 
-restore_vscodium_configuration
 restore_tlp_configuration
+restore_vscode_configuration
 setup_virtualization
+increase_file_watch_limit
 
 remove_bloat_packages
 rebuild_font_cache
